e83e7b2c28266b1ad85058a8679c67c9189e811c
Nareshkatika <aryanaresh999@gmail.com>
2024-05-17 09:42:49 +0000

    commit1

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,26 @@
+// Write your JS code here
+import {Link} from 'react'
+import './index.css'
+
+const Solution = props => {
+  const {items} = props
+  const {title, id, imageUrl, avatarUrl, author, topic} = items
+
+  return (
+    <li className="listEl">
+      <Link to={`/blogs/${id}`}>
+        <img className="imageEl1" alt={title} src={imageUrl} />
+        <div className="colArrangeEl1">
+          <p>{topic}</p>
+          <p>{title}</p>
+          <div className="rowArrangeEl1">
+            <img className="avatarEl" alt={author} src={avatarUrl} />
+            <p className="authorel">{author}</p>
+          </div>
+        </div>
+      </Link>
+    </li>
+  )
+}
+
+export default Solution
---
+++ src/components/BlogList/index.js
@@ -0,0 +1,44 @@
+// Write your JS code here
+import {useState, useEffect} from 'react'
+import Loader from 'react-loader-spinner'
+import Solution from '../BlogItem'
+
+import './index.css'
+
+const BlogList = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const getBlogsData = async () => {
+      const response = await fetch('https://apis.ccbp.in/blogs')
+      const data = await response.json()
+      const formattedData = data.map(eachItem => ({
+        id: eachItem.id,
+        title: eachItem.title,
+        imageUrl: eachItem.image_url,
+        avatarUrl: eachItem.avatar_url,
+        author: eachItem.author,
+        topic: eachItem.topic,
+      }))
+      setLoading((loading: false))
+    }
+  }, [])
+
+  return (
+    <div>
+      {loading ? (
+        <div data-testid="loader">
+          <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+        </div>
+      ) : (
+        <ul>
+          {getBlogsData.map(eachSol => (
+            <Solution key={eachSol.id} items={eachSol} />
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export default BlogList
